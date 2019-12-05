b861945195752d723d6edffc3119081f5cdb014d
gabegrimley <ggrimley@foretees.com>
2019-12-05 08:18:13 -0600

    Day 3 Cleanup

--- 3/solution.js
+++ 3/solution.js
@@ -6,251 +6,224 @@ module.exports = {
     //var text = fs.readFileSync('input2.txt').toString(); //test input
     var newLineSep = text.split(/\r?\n/);
     return newLineSep;
-}, mainFun: function(){
-    var inputs = this.readTextFile();
-    
-    var listOfLines = [];
-    for (var input = 0; input < 2; input++) {
+    }, mainFun: function(){
+        var inputs = this.readTextFile();
+        var listOfLines = [];
+        for (var input = 0; input < 2; input++) {
+            var sep = inputs[input].split(',');
+            var lastX = 0;
+            var lastY = 0;
+            for (var i = 0; i < sep.length; i++) {
+                var value = Number.parseInt(sep[i].substring(1));
+                switch(sep[i].substr(0, 1)) {
+                    case 'R':
+                        for (var v = lastX; v < lastX + value; v++) {
+                            listOfLines.push(input + ': ' + v + ',' + lastY);
+                        }
+                        lastX = value+lastX;
+                        break;
+                    case 'L': 
+                        for (var v = lastX; v > lastX - value; v--) {
+                            listOfLines.push(input + ': ' + v + ',' + lastY);
+                        }
+                        lastX = lastX - value;
+                        break;
+                    case 'D':
+                        for (var v = lastY; v > lastY - value; v--) {
+                            listOfLines.push(input + ': ' + lastX + ',' + v);
+                        }
+                        lastY = lastY - value;
+                        break;
+                    case 'U':
+                        for (var v = lastY; v < lastY + value; v++) {
+                            listOfLines.push(input + ': ' + lastX + ',' + v);
+                        }
+                        lastY = value+lastY;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        var sum = 0;
+        var sum2;
+        var a = [], b = [];
+
+        for (var x = 0; x < listOfLines.length; x++) {
+            if (listOfLines[x].substr(0,1) == 0) {
+                a.push(listOfLines[x].substring(3));
+            } else {
+                b.push(listOfLines[x].substring(3));
+            }
+        }
+
+        for (var i = 0; i < b.length; i++) {
+            if (a.includes(b[i])) {
+                if (b[i] != '0,0') {
+                    if (sum === 0) {
+                        sum = Math.abs(Number.parseInt(b[i].split(',')[0])) + Math.abs(Number.parseInt(b[i].split(',')[1]));
+                    } else {
+                        sum2 = Math.abs(Number.parseInt(b[i].split(',')[0])) + Math.abs(Number.parseInt(b[i].split(',')[1]));
+                        if (sum2 < sum) {
+                            sum = sum2;
+                        } 
+                    }
+                }
+            }
+        }
+        return sum;
+    }, findIntersection: function(intersection, input) {
+        var inputs = this.readTextFile();
+        var stepCnt = 0;
+        var done = false;
         var sep = inputs[input].split(',');
         var lastX = 0;
         var lastY = 0;
         for (var i = 0; i < sep.length; i++) {
+            done = false;
             var value = Number.parseInt(sep[i].substring(1));
             switch(sep[i].substr(0, 1)) {
                 case 'R':
                     for (var v = lastX; v < lastX + value; v++) {
-                        listOfLines.push(input + ': ' + v + ',' + lastY);
+                        if((v + ',' + lastY) == intersection) {
+                            done = true;
+                            stepCnt += Math.abs(v - lastX);
+                            return stepCnt;
+                        } 
+                    }
+                    if (done) {
+                        break;
+                    } else {
+                        stepCnt += value;
                     }
                     lastX = value+lastX;
                     break;
                 case 'L': 
                     for (var v = lastX; v > lastX - value; v--) {
-                        listOfLines.push(input + ': ' + v + ',' + lastY);
+                        if((v + ',' + lastY) == intersection) {
+                            done = true;
+                            stepCnt += Math.abs(v - lastX);
+                            return stepCnt;
+                        } 
+                    }
+                    if (done) {
+                        break;
+                    } else {
+                        stepCnt += value;
                     }
                     lastX = lastX - value;
                     break;
                 case 'D':
                     for (var v = lastY; v > lastY - value; v--) {
-                        listOfLines.push(input + ': ' + lastX + ',' + v);
+                        if((lastX + ',' + v) == intersection) {
+                            done = true;
+                            stepCnt += Math.abs(v - lastY);
+                            return stepCnt;
+                        } 
+                    }
+                    if (done) {
+                        break;
+                    } else {
+                        stepCnt += value;
                     }
                     lastY = lastY - value;
                     break;
                 case 'U':
                     for (var v = lastY; v < lastY + value; v++) {
-                        listOfLines.push(input + ': ' + lastX + ',' + v);
+                        if((lastX + ',' + v) == intersection) {
+                            done = true;
+                            stepCnt += Math.abs(v - lastY);
+                            return stepCnt;
+                        }
+                    }
+                    if (done) {
+                        break; 
+                    } else {
+                        stepCnt += value;
                     }
                     lastY = value+lastY;
                     break;
                 default:
                     break;
             }
+            
         }
-    }
-
-    //return listOfLines.length;
-
-    var sum = 0;
-    var sum2;
-    var a = [], b = [];
-
-    for (var x = 0; x < listOfLines.length; x++) {
-        if (listOfLines[x].substr(0,1) == 0) {
-            a.push(listOfLines[x].substring(3));
-        } else {
-            b.push(listOfLines[x].substring(3));
-        }
-    }
-
-    for (var i = 0; i < b.length; i++) {
-        if (a.includes(b[i])) {
-            if (b[i] != '0,0') {
-                if (sum === 0) {
-                    sum = Math.abs(Number.parseInt(b[i].split(',')[0])) + Math.abs(Number.parseInt(b[i].split(',')[1]));
-                } else {
-                    sum2 = Math.abs(Number.parseInt(b[i].split(',')[0])) + Math.abs(Number.parseInt(b[i].split(',')[1]));
-                    if (sum2 < sum) {
-                        sum = sum2;
-                    } 
-                }
-            }
-        }
-    }
-    return sum;
-}, findIntersection: function(intersection, input) {
-    var inputs = this.readTextFile();
-    var stepCnt = 0;
-    var done = false;
-    var sep = inputs[input].split(',');
-    var lastX = 0;
-    var lastY = 0;
-    var testInts = [];
-    for (var i = 0; i < sep.length; i++) {
-        done = false;
-        var value = Number.parseInt(sep[i].substring(1));
-        switch(sep[i].substr(0, 1)) {
-            case 'R':
-                for (var v = lastX; v < lastX + value; v++) {
-                   
-                    if((v + ',' + lastY) == intersection) {
-                        done = true;
-                        stepCnt += Math.abs(v - lastX);
-                        return stepCnt;
-                        break;
-                    } 
-                }
-                if (done) {
-                    break;
-                    //return stepCnt;
-                 } else {
-                     stepCnt += value;
-                 }
-                lastX = value+lastX;
-                
-                break;
-            case 'L': 
-                for (var v = lastX; v > lastX - value; v--) {
-                    if((v + ',' + lastY) == intersection) {
-                        done = true;
-                        stepCnt += Math.abs(v - lastX);
-                        return stepCnt;
-                        break;
-                    } 
-                }
-                if (done) {
-                    break;
-                    //return stepCnt;
-                } else {
-                    stepCnt += value;
-                }
-                lastX = lastX - value;
-                
-                break;
-            case 'D':
-                for (var v = lastY; v > lastY - value; v--) {
-                    if((lastX + ',' + v) == intersection) {
-                        done = true;
-                        stepCnt += Math.abs(v - lastY);
-                        return stepCnt;
-                        break;
-                    } 
-                }
-                if (done) {
-                    break;
-                    //return stepCnt;
-                 } else {
-                     stepCnt += value;
-                 }
-                
-                lastY = lastY - value;
-                break;
-            case 'U':
-                for (var v = lastY; v < lastY + value; v++) {
-                    if((lastX + ',' + v) == intersection) {
-                        done = true;
-                        stepCnt += Math.abs(v - lastY);
-                        return stepCnt;
-                        break;
-                    }
-                }
-                if (done) {
-                    break;
-                    //return stepCnt;
-                 } else {
-                     stepCnt += value;
-                 }
-                
-                lastY = value+lastY;
-                break;
-            default:
-                break;
-        }
+    return null;
         
-    }
-   return testInts;
-return stepCnt;
-    
-}, mainFunP2: function(){
-    var inputs = this.readTextFile();
-    
-    var listOfLines = [];
-    for (var input = 0; input < 2; input++) {
-        var sep = inputs[input].split(',');
-        var lastX = 0;
-        var lastY = 0;
-        for (var i = 0; i < sep.length; i++) {
-            var value = Number.parseInt(sep[i].substring(1));
-            switch(sep[i].substr(0, 1)) {
-                case 'R':
-                    for (var v = lastX; v < lastX + value; v++) {
-                        listOfLines.push(input + ': ' + v + ',' + lastY);
-                    }
-                    lastX = value+lastX;
-                    break;
-                case 'L': 
-                    for (var v = lastX; v > lastX - value; v--) {
-                        listOfLines.push(input + ': ' + v + ',' + lastY);
-                    }
-                    lastX = lastX - value;
-                    break;
-                case 'D':
-                    for (var v = lastY; v > lastY - value; v--) {
-                        listOfLines.push(input + ': ' + lastX + ',' + v);
-                    }
-                    lastY = lastY - value;
-                    break;
-                case 'U':
-                    for (var v = lastY; v < lastY + value; v++) {
-                        listOfLines.push(input + ': ' + lastX + ',' + v);
-                    }
-                    lastY = value+lastY;
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    var intersections = [];
-    var sum = 0;
-    var sum2;
-    var a = [], b = [];
-
-    for (var x = 0; x < listOfLines.length; x++) {
-        if (listOfLines[x].substr(0,1) == 0) {
-            a.push(listOfLines[x].substring(3));
-        } else {
-            b.push(listOfLines[x].substring(3));
-        }
-    }
-
-    for (var i = 0; i < b.length; i++) {
-        if (a.includes(b[i])) {
-            if (b[i] != '0,0') {
-                if (!intersections.includes(b[i])) {
-                    intersections.push(b[i]);
+    }, mainFunP2: function(){
+        var inputs = this.readTextFile();
+        var listOfLines = [];
+        for (var input = 0; input < 2; input++) {
+            var sep = inputs[input].split(',');
+            var lastX = 0;
+            var lastY = 0;
+            for (var i = 0; i < sep.length; i++) {
+                var value = Number.parseInt(sep[i].substring(1));
+                switch(sep[i].substr(0, 1)) {
+                    case 'R':
+                        for (var v = lastX; v < lastX + value; v++) {
+                            listOfLines.push(input + ': ' + v + ',' + lastY);
+                        }
+                        lastX = value+lastX;
+                        break;
+                    case 'L': 
+                        for (var v = lastX; v > lastX - value; v--) {
+                            listOfLines.push(input + ': ' + v + ',' + lastY);
+                        }
+                        lastX = lastX - value;
+                        break;
+                    case 'D':
+                        for (var v = lastY; v > lastY - value; v--) {
+                            listOfLines.push(input + ': ' + lastX + ',' + v);
+                        }
+                        lastY = lastY - value;
+                        break;
+                    case 'U':
+                        for (var v = lastY; v < lastY + value; v++) {
+                            listOfLines.push(input + ': ' + lastX + ',' + v);
+                        }
+                        lastY = value+lastY;
+                        break;
+                    default:
+                        break;
                 }
             }
         }
-    }
-   //return intersections[0];
 
-    //traverse each a and b to find retVal, get index. Count values up until then
-    var intersectionsSteps = [];
-    var shortestStep = 100000000;
-    var steps = [];
-    for (var i = 0; i < intersections.length; i++) {
-        var stepCount = 0;
-        //return this.findIntersection(intersections[i], 0);
-        stepCount = this.findIntersection(intersections[i], 0) + this.findIntersection(intersections[i], 1);
-       // steps.push('[' + this.findIntersection(intersections[i], 0) + ',' + this.findIntersection(intersections[i], 1) + ']');
-        steps.push(stepCount);
-        if (stepCount < shortestStep) {
-            shortestStep = stepCount;
+        var intersections = [];
+        var a = [], b = [];
+
+        for (var x = 0; x < listOfLines.length; x++) {
+            if (listOfLines[x].substr(0,1) == 0) {
+                a.push(listOfLines[x].substring(3));
+            } else {
+                b.push(listOfLines[x].substring(3));
+            }
         }
-    }
 
-    return shortestStep;
-}
+        for (var i = 0; i < b.length; i++) {
+            if (a.includes(b[i])) {
+                if (b[i] != '0,0') {
+                    if (!intersections.includes(b[i])) {
+                        intersections.push(b[i]);
+                    }
+                }
+            }
+        }
+
+        var shortestStep = 100000000;
+        var steps = [];
+        for (var i = 0; i < intersections.length; i++) {
+            var stepCount = 0; 
+            stepCount = this.findIntersection(intersections[i], 0) + this.findIntersection(intersections[i], 1);
+            steps.push(stepCount);
+            if (stepCount < shortestStep) {
+                shortestStep = stepCount;
+            }
+        }
+        return shortestStep;
+    }
 }
 
 
